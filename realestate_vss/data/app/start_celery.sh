98f72b5710d666cf30e17a5e7e78a7865413a25e
Kelvin Chan <kechan@gmail.com>
2024-12-09 18:37:15 -0500

    concurreny 2

--- realestate_vss/data/app/start_celery.sh
+++ realestate_vss/data/app/start_celery.sh
@@ -23,7 +23,8 @@ celery -A celery_unstack.celery worker \
   --logfile=celery_unstack.log \
   -Q unstack_queue \
   --detach \
-  --hostname=unstack_worker@%h
+  --hostname=unstack_worker@%h \
+  --concurrency=2
 
 #celery -A celery_embed.celery worker --loglevel=info --logfile=celery_embed.log -P solo -Q embed_queue --detach --hostname=embed_worker@%h 
 #celery -A celery_update_embeddings.celery worker --loglevel=info --logfile=celery_update_embeddings.log -P solo -Q update_embed_queue --detach --hostname=update_embeddings_worker@%h
